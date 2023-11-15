5ef3c6a05960d2884857ad0baa636a98b32c1e65
unknown <newraygos@gmail.com>
2023-11-15 22:36:32 +0300

    feat: complete 2 task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -40,8 +40,12 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  let x;
+  if (typeof value === 'string') x = true;
+  else if (value instanceof String) x = true;
+  else x = false;
+  return x;
 }
 
 /**
